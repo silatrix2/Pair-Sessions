bb2581b6e754cf6d4586742560ef2ff11b9be485
silatrix2 <silatrix22@gmail.com>
2025-09-24 11:28:35 +0300

    Update gifted.js

--- gifted.js
+++ gifted.js
@@ -1,10 +1,19 @@
-const express = require('express');
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import code from './pair.js';
+import events from 'events';
+
 const app = express();
-const path = require('path');
-const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let code = require('./pair');
-require('events').EventEmitter.defaultMaxListeners = 500;
+
+// __dirname replacement in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+events.EventEmitter.defaultMaxListeners = 500;
 
 app.use('/code', code);
 
@@ -18,7 +27,7 @@ app.get('/pair', (req, res) => {
   res.sendFile(path.join(__dirname, 'pair.html'));
 });
 
-// Serve 'qr.html' when visiting '/qr.js' route
+// Serve 'qr.html' when visiting '/qr' route
 app.get('/qr', (req, res) => {
   res.sendFile(path.join(__dirname, 'qr.html'));
 });
@@ -26,11 +35,11 @@ app.get('/qr', (req, res) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`
 Deployment Successful!
 
 Silva-Session-Server Running on http://localhost:` + PORT)
 });
 
-module.exports = app;
+export default app;
