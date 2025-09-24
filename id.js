28a3bbac49506ca39c55db85b0294d0b82a670ed
silatrix2 <silatrix22@gmail.com>
2025-09-24 11:40:51 +0300

    Update id.js

--- id.js
+++ id.js
@@ -1,10 +1,12 @@
-function giftedid(num = 4) {
+// id.js
+export function giftedid(num = 4) {
   let result = "";
   let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var characters9 = characters.length;
-  for (var i = 2; i < num; i++) {
+  let characters9 = characters.length;
+  
+  for (let i = 2; i < num; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters9));
   }
+  
   return result;
 }
-module.exports = {giftedid};
